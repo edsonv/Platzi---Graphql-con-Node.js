1dffddaa89e1563924e0664f967591a1498f75c6
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-30 17:23:24 -0500

    Clase 15 - Update and Delete

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,4 +1,10 @@
-const { getProduct, getProducts, addProduct } = require('./product.resolvers');
+const {
+  getProduct,
+  getProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+} = require('./product.resolvers');
 
 const resolvers = {
   Query: {
@@ -16,6 +22,8 @@ const resolvers = {
   },
   Mutation: {
     addProduct,
+    updateProduct,
+    deleteProduct,
   },
 };
 
