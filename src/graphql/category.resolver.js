56c9d8e91159ee46dde6a38f6bdc8525bc001ca8
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-31 14:22:44 -0500

    Clase 19 - Graphql con JWT

--- src/graphql/category.resolver.js
+++ src/graphql/category.resolver.js
@@ -1,7 +1,13 @@
 const CategoryService = require('./../services/category.service');
 const service = new CategoryService();
+const boom = require('@hapi/boom');
 
-const addCategory = (_, { dto }) => {
+const addCategory = async (_, { dto }, context) => {
+  const { user } = await context.authenticate('jwt', { session: false });
+  if (!user) {
+    throw boom.unauthorized('JWT is not valid');
+  }
+
   return service.create(dto);
 };
 
